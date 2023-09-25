ffe04eb6f46fa45e564a5c72c5534bc6e7114e55
DODDA BHARGAV <128250258+Bhargav-Dodda-07@users.noreply.github.com>
2023-09-25 21:18:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+ // Function to execute when the DOM is loaded
+        function onDOMLoad() {
+            // Get the body element
+            const body = document.body;
+
+            // Remove any existing child nodes from the body
+            while (body.firstChild) {
+                body.removeChild(body.firstChild);
+            }
+
+            // Create a text node with the message
+            const textNode = document.createTextNode("DOM load success");
+
+            // Append the text node to the body
+            body.appendChild(textNode);
+        }
+
+        // Add an event listener for the DOMContentLoaded event
+        document.addEventListener("DOMContentLoaded", onDOMLoad);
